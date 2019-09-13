lcoenen/open-enterprise

commit 3078cee77f386bc447c77d9b76a70754e08d8e2e

Dot Voting: Move tokenContract def to VoteDetails

apps/dot-voting/app/components/VoteDetails/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { BigNumber } from 'bignumber.js'
 import { Box, Button, GU, Split, Text } from '@aragon/ui'
-import { combineLatest } from 'rxjs'
+import { useAragonApi } from '@aragon/api-react'
 import { first } from 'rxjs/operators' // Make sure observables have .first
 import AppBadge from './AppBadge'
 import Status from './Status'
@@ -12,8 +12,12 @@ import VotingResults from './VotingResults'
 import CastVote from './CastVote'
 import Participation from './Participation'
 import Label from './Label'
+import tokenDecimalsAbi from '../../abi/token-decimals.json'
 
-const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
+const tokenAbi = [].concat(tokenDecimalsAbi)
+
+const VoteDetails = ({ vote, onVote }) => {
+  const { api, appState: { tokenAddress = '' }, connectedAccount } = useAragonApi()
   const [ votingMode, setVotingMode ] = useState(false)
   const [ voteWeights, setVoteWeights ] = useState([])
   const [ canIVote, setCanIVote ] = useState(false)
@@ -26,10 +30,15 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
     type,
   } = vote.data
 
+  const getTokenContract = tokenAddress =>
+    tokenAddress && api.external(tokenAddress, tokenAbi)
+
+  const tokenContract = getTokenContract(tokenAddress)
+
   useEffect(() => {
     async function getVoteWeights() {
-      const result = await app
-        .call('getVoterState', voteId, userAccount)
+      const result = await api
+        .call('getVoterState', voteId, connectedAccount)
         .toPromise()
 
       const totalVotesCount = result.reduce(
@@ -47,9 +56,9 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
     }
 
     function canIVote() {
-      if (userAccount && vote) {
-        app
-          .call('canVote', voteId, userAccount)
+      if (connectedAccount && vote) {
+        api
+          .call('canVote', voteId, connectedAccount)
           .pipe(first())
           .subscribe(canVote => {
             setCanIVote(canVote)
@@ -58,7 +67,7 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
     }
 
     function loadDecimals() {
-      if (tokenContract && userAccount) {
+      if (tokenContract && connectedAccount) {
         tokenContract.decimals()
           .subscribe(decimals => {
             setDecimals(decimals)
@@ -69,7 +78,7 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
     getVoteWeights()
     canIVote()
     loadDecimals()
-  }, [ vote, userAccount ])
+  }, [ vote, connectedAccount ])
 
 
   // eslint-disable-next-line react/prop-types
@@ -122,7 +131,7 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
               <CastVote
                 onVote={onVote}
                 toggleVotingMode={toggleVotingMode}
-                userAccount={userAccount}
+                connectedAccount={connectedAccount}
                 vote={vote}
                 voteWeights={voteWeights}
               />
@@ -147,9 +156,6 @@ const VoteDetails = ({ app, vote, tokenContract, userAccount, onVote }) => {
 }
 
 VoteDetails.propTypes = {
-  app: PropTypes.object.isRequired,
-  userAccount: PropTypes.string.isRequired,
-  tokenContract: PropTypes.object.isRequired,
   vote: PropTypes.object.isRequired,
   onVote: PropTypes.func.isRequired,
 }

apps/dot-voting/app/Decisions.js
@@ -14,10 +14,6 @@ import {
   VOTE_STATUS_FAILED,
   VOTE_STATUS_SUCCESSFUL,
 } from './utils/vote-types'
-import tokenBalanceOfAbi from './abi/token-balanceof.json'
-import tokenDecimalsAbi from './abi/token-decimals.json'
-
-const tokenAbi = [].concat(tokenBalanceOfAbi, tokenDecimalsAbi)
 
 const useFilterVotes = (votes, voteTime) => {
   const [ filteredVotes, setFilteredVotes ] = useState(votes)
@@ -108,19 +104,10 @@ const useFilterVotes = (votes, voteTime) => {
 
 const Decisions = ({ decorateVote }) => {
   const { api: app, appState, connectedAccount } = useAragonApi()
-  const {
-    votes,
-    voteTime,
-    tokenAddress = '',
-  } = appState
+  const { votes, voteTime } = appState
 
   const { layoutName } = useLayout()
   const theme = useTheme()
-
-  const getTokenContract = (tokenAddress) =>
-    tokenAddress && app.external(tokenAddress, tokenAbi)
-
-  const tokenContract = getTokenContract(tokenAddress)
 
   // TODO: accomplish this with routing (put routes in App.js, not here)
   const [ currentVoteId, setCurrentVoteId ] = useState(-1)
@@ -161,13 +148,7 @@ const Decisions = ({ decorateVote }) => {
         <Bar>
           <BackButton onClick={handleBackClick} />
         </Bar>
-        <VoteDetails
-          app={app}
-          vote={currentVote}
-          userAccount={connectedAccount}
-          tokenContract={tokenContract}
-          onVote={handleVote}
-        />
+        <VoteDetails vote={currentVote} onVote={handleVote} />
       </React.Fragment>
     )
   }

commit 4463c86e51a9eb3affe108c8df26402e4ab5cd55

Dot Voting: fix CastVote connectedAccount logic
This fixes a regression introduced by 3078cee77f386bc447c77d9b76a70754e08d8e2e

Rather than getting `userAccount` from the parent component, CastVote
now fetches `connectedAccount` directly from `useAragonApi`

apps/dot-voting/app/components/VoteDetails/CastVote.js
@@ -26,14 +26,8 @@ const SliderAndValueContainer = styled.div`
   width: 100%;
 `
 
-const CastVote = ({
-  onVote,
-  toggleVotingMode,
-  userAccount,
-  vote,
-  voteWeights,
-}) => {
-  const { api, appState: { tokenAddress } } = useAragonApi()
+const CastVote = ({ onVote, toggleVotingMode, vote, voteWeights }) => {
+  const { api, appState: { tokenAddress }, connectedAccount } = useAragonApi()
 
   const theme = useTheme()
 
@@ -62,9 +56,9 @@ const CastVote = ({
 
     function loadUserBalance() {
       const tokenContract = tokenAddress && api.external(tokenAddress, tokenBalanceOfAbi)
-      if (tokenContract && userAccount) {
+      if (tokenContract && connectedAccount) {
         combineLatest(
-          tokenContract.balanceOfAt(userAccount, vote.data.snapshotBlock)
+          tokenContract.balanceOfAt(connectedAccount, vote.data.snapshotBlock)
         )
           .pipe(first())
           .subscribe(([userBalance]) => {
@@ -75,7 +69,7 @@ const CastVote = ({
 
     getVoterState()
     loadUserBalance()
-  }, [ vote, userAccount ])
+  }, [ vote, connectedAccount ])
 
   const sliderUpdate = useCallback((value, idx) => {
     const total = voteOptions.reduce(
@@ -184,7 +178,6 @@ const CastVote = ({
 CastVote.propTypes = {
   onVote: PropTypes.func.isRequired,
   toggleVotingMode: PropTypes.func.isRequired,
-  userAccount: PropTypes.string.isRequired,
   vote: PropTypes.shape({
     data: PropTypes.shape({
       options: PropTypes.PropTypes.arrayOf(PropTypes.object).isRequired,

apps/dot-voting/app/components/VoteDetails/index.js
@@ -138,7 +138,6 @@ const VoteDetails = ({ vote, onVote }) => {
               <CastVote
                 onVote={onVote}
                 toggleVotingMode={toggleVotingMode}
-                connectedAccount={connectedAccount}
                 vote={vote}
                 voteWeights={voteWeights}
               />